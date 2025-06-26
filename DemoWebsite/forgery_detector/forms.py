a3d8aa83600fe4b204a0b8153aae8453f7badc22
Chih-Yu Jian <32098079+IlikeBB@users.noreply.github.com>
2025-06-26 14:17:34 +0800

    Update forms.py

--- DemoWebsite/forgery_detector/forms.py
+++ DemoWebsite/forgery_detector/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 
+from django.conf import settings
 class FaceSwapForm(forms.Form):
     src = forms.ImageField(label="來源圖片 A")
     dst = forms.ImageField(label="目標圖片 B")
+
+class DiffDetectForm(forms.Form):
+    diff_img = forms.ImageField(label='選擇檢測圖片')
+    model_choice = forms.ChoiceField(
+        choices=settings.DETECT_MODEL_CHOICES,
+        initial=settings.DEFAULT_DETECT_MODEL,
+        label='選擇檢測模型'
+    )
