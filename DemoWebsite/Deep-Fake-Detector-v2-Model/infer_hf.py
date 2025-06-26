f6c73ed740ae1690308b79e1745cc2cc15a4a50d
Chih-Yu Jian <32098079+IlikeBB@users.noreply.github.com>
2025-06-26 14:27:05 +0800

    Update infer_hf.py

--- DemoWebsite/Deep-Fake-Detector-v2-Model/infer_hf.py
+++ DemoWebsite/Deep-Fake-Detector-v2-Model/infer_hf.py
@@ -4,31 +4,23 @@ from PIL import Image
 from torchvision import transforms
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 
-# 1. 指定模型
 HF_MODEL = "prithivMLmods/open-deepfake-detection"
 
-# 2. 准备 ImageProcessor（负责 Resize/Crop/Normalize）
 processor = AutoImageProcessor.from_pretrained(HF_MODEL)
 
-# 3. 加载模型（会自动下载权重到本地缓存）
 model = AutoModelForImageClassification.from_pretrained(HF_MODEL)
 model.eval()
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
-# 4. 推理函数
 def predict(image_path: str):
-    # 读图
     img = Image.open(image_path).convert("RGB")
-    # 预处理：会自动做 Resize→CenterCrop→ToTensor→Normalize
     inputs = processor(images=img, return_tensors="pt").to(device)
     with torch.no_grad():
         logits = model(**inputs).logits  # shape [1, 2]
         probs  = torch.softmax(logits[0], dim=0).cpu().tolist()
-    # 标签映射见 Model Card：id 0 = Realism，1 = Deepfake
     return {"p_real": probs[0], "p_fake": probs[1]}
 
-# 5. 测试
 if __name__ == "__main__":
     real_img = "/ssd7/chih/DeepFake_WebDemo/deepfake_site/media/diff_03b91026e9d8442da292b806c75ffbd6.png"
     fake_img = "/ssd7/chih/DeepFake_WebDemo/deepfake_site/media/diff_67912971e8124b6fb6aaa7aab24b32c0.jpg"
